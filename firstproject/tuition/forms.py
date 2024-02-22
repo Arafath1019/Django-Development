e0990f9cf4b8a8b3b0f41fdbb3ebce81e738491e
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-02-22 14:21:30 +0600

    feat: django ModelForm

--- firstproject/tuition/forms.py
+++ firstproject/tuition/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from .models import Contact
 
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label="Your Name")
-    phone = forms.CharField(max_length=100, label="Your Phone")
-    content = forms.CharField(max_length=100, label="Your content")
+# class ContactForm(forms.Form):
+#     name = forms.CharField(max_length=100, label="Your Name")
+#     phone = forms.CharField(max_length=100, label="Your Phone")
+#     content = forms.CharField(max_length=100, label="Your content")
     
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
--- firstproject/tuition/views.py
+++ firstproject/tuition/views.py
@@ -7,12 +7,14 @@ from .forms import ContactForm
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
+        # if form.is_valid():
+        #     name = form.cleaned_data['name']
+        #     phone = form.cleaned_data['phone']
+        #     content = form.cleaned_data['content']
+        #     obj = Contact(name=name, phone=phone, content=content)
+        #     obj.save()
         if form.is_valid():
-            name = form.cleaned_data['name']
-            phone = form.cleaned_data['phone']
-            content = form.cleaned_data['content']
-            obj = Contact(name=name, phone=phone, content=content)
-            obj.save()
+            form.save()
     else:
         form = ContactForm()
     return render(request, 'contact.html', {'form': form})
